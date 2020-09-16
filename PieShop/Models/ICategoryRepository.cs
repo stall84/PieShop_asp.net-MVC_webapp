edd6df4a667239f2db263ea8a656286f6b78b581
Michael Stallings <stall84@gmail.com>
2020-09-16 10:19:49 -0400

    added Interface, Repository and MockRepository classes to Models folder

---
+++ PieShop/Models/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PieShop.Models
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> AllCategories { get; }
+    }
+}
--- PieShop/Startup.cs
+++ PieShop/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PieShop.Models;
 
 namespace PieShop
 {
@@ -17,6 +18,10 @@ namespace PieShop
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddScoped<IPieRepository, MockPieRepository>();
+            services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
